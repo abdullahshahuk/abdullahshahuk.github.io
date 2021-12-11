c48b5cbe97637cc1744a672d956196cf464905c7
Abdullah Shah <42191357+Abzzdev@users.noreply.github.com>
2021-12-11 13:17:10 +0000

    Moved script to js file added link and mailto

--- script.js
+++ script.js
@@ -51,3 +51,65 @@ window.onload = function () {
         }
     }
 };
+
+/// Drawers
+
+var contactSection = document.getElementById('contactSection');
+var aboutSection = document.getElementById('aboutSection');
+var main = document.getElementById('main');
+var contactToggled = true;
+var aboutToggled = true;
+var mainToggled = false;
+
+document.getElementById('contact').onclick = function () {
+    if (contactToggled == false) {
+        contactSection.classList.toggle('nofade');
+        contactSection.classList.toggle('fade');
+        if (mainToggled == true && aboutToggled == true) {
+            mainToggled = false;
+            console.log(mainToggled);
+            main.classList.toggle('fade');
+            main.classList.toggle('nofade');
+        }
+        contactToggled = true;
+        console.log("Fading out contact section");
+    }
+    else if (contactToggled == true) {
+        contactSection.classList.toggle('fade');
+        contactSection.classList.toggle('nofade');
+        if (mainToggled == false) {
+            mainToggled = true;
+            main.classList.toggle('nofade');
+            main.classList.toggle('fade');
+        }
+        contactToggled = false;
+        console.log("Fading in contact section");
+    }
+};
+
+document.getElementById('about').onclick = function () {
+    if (aboutToggled == false) {
+        aboutSection.classList.toggle('nofade');
+        aboutSection.classList.toggle('fade');
+        if (mainToggled == true && contactToggled == true) {
+            mainToggled = false;
+            console.log(mainToggled);
+            main.classList.toggle('fade');
+            main.classList.toggle('nofade');
+        }
+        aboutToggled = true;
+        console.log("Fading out about section");
+    }
+    else if (aboutToggled == true) {
+        if (mainToggled == false) {
+            mainToggled = true;
+            console.log(mainToggled);
+            main.classList.toggle('nofade');
+            main.classList.toggle('fade');
+        }
+        aboutSection.classList.toggle('fade');
+        aboutSection.classList.toggle('nofade');
+        aboutToggled = false;
+        console.log("Fading in about section");
+    }
+};
